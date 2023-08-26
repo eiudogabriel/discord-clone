7c28c69cc689d1c1dc1216435e635a484f4af3d1
Eiudo Gabriel <eiudogabriel@gmail.com>
2023-08-26 13:14:52 -0300

    fix: background color for dark and light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Space_Grotesk } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
 import { ThemeProvider } from '@/components/providers/theme-provider'
+import { cn } from '@/lib/utils'
 
 const font = Space_Grotesk({ subsets: ['latin'] })
 
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={font.className}>
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false} storageKey='discord-theme'>
             {children}
           </ThemeProvider>
